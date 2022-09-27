577e9107d067e591bd41de188825b5ee287a7e33
JoeDirrtyy <joeymarkowski96@gmail.com>
2022-09-26 22:50:52 -0400

    Person Handler

---
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -0,0 +1,92 @@
+package com.zipcodewilmington;
+
+
+/**
+ * Created by leon on 1/24/18.
+ */
+public class PersonHandler {
+    private final Person[] personArray;
+
+    public PersonHandler(Person[] personArray) {
+        this.personArray = personArray;
+    }
+
+    public String whileLoop() {
+        String result = "";
+        int counter = 0;
+        while(counter < personArray.length){
+            Person person = personArray[counter];
+            person.toString();
+            result = result + person.toString();
+            counter++;
+        }
+
+
+        // create a `counter`
+        // while `counter` is less than length of array
+        // begin loop
+
+        // use `counter` to identify the `current Person` in the array
+        // get `string Representation` of `currentPerson`
+        // append `stringRepresentation` to `result` variable
+
+        // end loop
+        return result;
+    }
+
+
+
+    public String forLoop() {
+        String result = "";
+        for(int counter = 0; counter < personArray.length; counter++){
+            Person person = personArray[counter];
+            person.toString();
+            result = result + person.toString();
+
+        }
+
+
+
+
+        // identify initial value
+        // identify terminal condition
+        // identify increment
+
+        // use the above clauses to declare for-loop signature
+        // begin loop
+        // use `counter` to identify the `current Person` in the array
+        // get `string Representation` of `currentPerson`
+        // append `stringRepresentation` to `result` variable
+        // end loop
+
+        return result;
+    }
+
+
+
+    public String forEachLoop() {
+        String result = "";
+
+        for(Person person: personArray){
+            result = result + person.toString();
+
+        }
+
+
+        // identify array's type
+        // identify array's variable-name
+
+        // use the above discoveries to declare for-each-loop signature
+        // begin loop
+        // get `string Representation` of `currentPerson`
+        // append `stringRepresentation` to `result` variable
+        // end loop
+
+        return result;
+    }
+
+
+    public Person[] getPersonArray() {
+        return personArray;
+    }
+}
